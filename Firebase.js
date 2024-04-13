9de944bc8d3c46264077635d0021b05e3e7c30dc
Mahsa Mirzadaei <m.mirzadaei@asiatech.ir>
2024-04-13 16:56:28 +0330

    connect firebase

---
+++ Firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBZKHZe3dTAnfN9RG8FAwU6JjlPNepy3Lw",
+  authDomain: "crypto-fb427.firebaseapp.com",
+  projectId: "crypto-fb427",
+  storageBucket: "crypto-fb427.appspot.com",
+  messagingSenderId: "510584029953",
+  appId: "1:510584029953:web:ddfbc63f7fda147fcf5100",
+  measurementId: "G-15BPD0EF43",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,53 @@
-import React from "react";
-
+"use client";
+import { Button, FormControl, Paper, TextField } from "@mui/material";
+import { auth } from "../../../Firebase";
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 const Login = () => {
-  return <div>Login</div>;
+  const [userData, setUserData] = useState({
+    email: "",
+    pass: "",
+  });
+
+  const handleLogin = async () => {
+    createUserWithEmailAndPassword(auth, userData.email, userData.pass).then(
+      (authUser) => {
+        console.log(authUser);
+      }
+    );
+  };
+
+  return (
+    <Paper sx={{ width: "fit-content", p: "2rem" }}>
+      <h1>Sign Up</h1>
+      <FormControl>
+        <TextField
+          required
+          label="Email"
+          sx={{ mb: "1rem" }}
+          onChange={(e) =>
+            setUserData((pre) => ({ ...pre, email: e.target.value }))
+          }
+        />
+        <TextField
+          required
+          label="Password"
+          type="password"
+          onChange={(e) =>
+            setUserData((pre) => ({ ...pre, pass: e.target.value }))
+          }
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          sx={{ mt: "1rem" }}
+          onClick={() => handleLogin()}
+        >
+          Submit
+        </Button>
+      </FormControl>
+    </Paper>
+  );
 };
 
 export default Login;
